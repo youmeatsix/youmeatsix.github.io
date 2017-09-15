8747a97823bcb83eb824cbce75c34b32a642e096
youmeatsix <31981423+youmeatsix@users.noreply.github.com>
2017-09-15 21:59:03 +0300

    Update response.php

--- response.php
+++ response.php
@@ -1,29 +1,3 @@
 <?php
-
-$curl = curl_init();
-$begin = "2017/01/01";
-$end = "2017/09/15";
-
-curl_setopt_array($curl, array(
-  CURLOPT_URL => "https://promo.fabiohome.ru/test",
-  CURLOPT_RETURNTRANSFER => true,
-  CURLOPT_ENCODING => "",
-  CURLOPT_MAXREDIRS => 10,
-  CURLOPT_TIMEOUT => 30,
-  CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-  CURLOPT_CUSTOMREQUEST => "POST",
-  CURLOPT_POSTFIELDS => "begin=".$begin."&end=".$end."",
-  CURLOPT_HTTPHEADER => array(
-    "cache-control: no-cache",
-    "content-type: application/x-www-form-urlencoded"
-  ),
-));
-
-$response = curl_exec($curl);
-$err = curl_error($curl);
-
-curl_close($curl);
-
-echo $response;
-
+    echo $_POST['begin'] . ', ' . $_POST['end'];
 ?>
